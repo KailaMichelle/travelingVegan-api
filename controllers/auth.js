ca7ef2343c3d7b1a133d344fcd50fbf801f48b0a
KailaMichelle <kaila22@hotmail.com>
2020-08-21 15:25:16 -0700

    added login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,8 @@ const jwt = require('jsonwebtoken');
 // json web token - used to authorize user that is sending requests (not to be confused with authentication that you use to log in) 
 const db = require('../models');
 
+
+// ************************************** SIGN  UP **************************************
 const signup = async (req, res) => {
     // return res.json({message: 'Register route working'})
     if(!req.body.username || !req.body.email || !req.body.password){
@@ -37,10 +39,43 @@ const signup = async (req, res) => {
     }
 };
 
+// ************************************** LOGIN **************************************
 const login = async (req, res) => {
-    return res.json({message: 'Login route working'})
-}
+    // return res.json({message: 'Login route working'})
+    try {
+        const foundUser = await db.User.findOne({ email: req.body.email });
+        if(!foundUser) {
+            return res.status(400).json({
+                status: 400,
+                message: 'Username or Password incorrect'
+            });
+        }
 
+        const isMatch = await bcrypt.compare(req.body.password, foundUser.password);
+        if(!isMatch){
+            return res.status(400).json({
+                status: 400,
+                message: 'Password is incorrect'
+            });
+        }
+
+        const payload = {id: foundUser._id};
+        const secret = process.env.JWT_SECRET;
+        const expiration = {expiresIn: '1h'};
+
+        const token = await jwt.sign(payload, secret, expiration);
+
+        res.status(200).json({token});
+    } catch(err) {
+        console.log(err);
+        return res.status(500).json({
+            status: 500,
+            message: 'Something went wrong, try again',
+        });
+    }
+};
+
+// ************************************** VERIFY **************************************
 const verify = async (req, res) => {
     return res.json({message: 'Verify route working'})
 }
