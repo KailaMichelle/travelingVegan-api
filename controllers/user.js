f48ca63c50b9bd527a93387d16d1b2db77e53a7e
KailaMichelle <kaila22@hotmail.com>
2020-08-21 16:46:41 -0700

    updates

--- controllers/user.js
+++ controllers/user.js
@@ -8,39 +8,39 @@ const index = (req, res) => {
     })
 }
 
-const show = (req, res) => {
-    db.User.findById(req.params.id, (err, foundUser) => {
-        if(err) console.log('Error in show', err);
+// const show = (req, res) => {
+//     db.User.findById(req.params.id, (err, foundUser) => {
+//         if(err) console.log('Error in show', err);
 
-        res.status(200).json(foundUser);
-    })
-}
+//         res.status(200).json(foundUser);
+//     })
+// }
 
-const update = (req, res) => {
-    db.User.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedUser) => {
-        if(err) console.log('Error in update', err);
+// const update = (req, res) => {
+//     db.User.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedUser) => {
+//         if(err) console.log('Error in update', err);
 
-        if (!updatedUser){
-            res.status(400).json({message: `Could not update ${req.params.id}`})
-        }
+//         if (!updatedUser){
+//             res.status(400).json({message: `Could not update ${req.params.id}`})
+//         }
 
-        res.json(updatedUser);
-    })
-}
+//         res.json(updatedUser);
+//     })
+// }
 
-const destroy = (req, res) => {
-    db.User.findByIdAndDelete(req.params.id, (err, deletedUser) => {
-        if(err) console.log('Error in delete', err);
+// const destroy = (req, res) => {
+//     db.User.findByIdAndDelete(req.params.id, (err, deletedUser) => {
+//         if(err) console.log('Error in delete', err);
 
-        res.status(200).json(deletedUser);
-    })
-}
+//         res.status(200).json(deletedUser);
+//     })
+// }
 
 module.exports ={
     index, 
-    show, 
-    update,
-    destroy
+    // show, 
+    // update,
+    // destroy
 }
 
 
