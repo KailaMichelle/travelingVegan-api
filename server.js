d71a84a3ac51330f8feb1b4d4185056a86668010
KailaMichelle <kaila22@hotmail.com>
2020-08-20 21:37:58 -0700

    added cors

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 // IMPORTS
 const express = require ('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const routes = require('./routes');
 const PORT = process.env.PORT;
 const app = express();
+
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    methods: "GET,POST,PUT,DELETE",
+    optionsSuccessStatus: 200
+}));
 
 // MIDDLEWARE - JSON parsing that allows AJAX requests (asynchronous javascript & XML)
 app.use(express.urlencoded({extended: false}));
